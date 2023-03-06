e3d15fa846a8dbd7945335ecffab733308136ab1
Robert Vidigal <rb5286@nyu.edu>
2023-03-06 12:23:50 -0500

    Class 2 Commit

--- IntroR_DEMO.R
+++ IntroR_DEMO.R
@@ -11,53 +11,39 @@
 # # -----------------------------------------------------------------------
 # --- Today RStudio Topics ---
 # # -----------------------------------------------------------------------
-### Variable types (entering data in R).
-### Data transformation (using R operators).
+### Entering data in R (variable types).
+### Using R operators with data frames (data structures).
 
 # # -----------------------------------------------------------------------
 # LET'S BEGIN!
 # # -----------------------------------------------------------------------
 rm(list=ls()) # Cleaning R Environment
 
-# The hash sign (#) is the character used to comment code. 
-# R ignores everything that follows this character until the end of the line. 
-
-# It is good practice to comment code, that is, to include short notes/messages 
-# to ourselves or to our collaborators explaining what the code does. 
-
-# This will make reading and understanding our code easier.
-
 # # -----------------------------------------------------------------------
 # ENTERING DATA IN R: VARIABLE TYPES
 # # -----------------------------------------------------------------------
 
 # There are multiple ways to enter/import data in R.
-# The simplest one is to  enter it manually.
+# The simplest way is to enter it manually.
 
 # --- Assignment Operators 
 # Remember: '<-' is used to assign data to a variable or data frame.
 # Remember: '$' is used to call variables in a data frame.
-
-# Operators example:
-# # -------------------------------------------------------------------------------------------
-df <- data.frame(column1 = c(1:10) , column2 = c(LETTERS[1:10])) # '<-' & ":" operators
-df
-df$column3<-c(10:1) # The '$' operator 
-df
 
 # --- Creating a data.frame object
 # # -------------------------------------------------------------------------------------------
 # Let's suppose we collected survey answers on some political, psychological, 
 # and demographic characteristics.
 
+### Inputting data manually
 minidf<-data.frame(
   "name"=c("Sofia", "Pedro", "Juan", "Emma", "Fred", "Sasha", "Rachel", "Erica", "Amanda", "Maya"), # Individuals
   "age"=c(23, 22, 44, 56, 90, 18, 77, 31, 20, 23), # Age in years
   "gender"=c(1, 0, 0, 1, 0, 0, 1, 1, 1, 2), # Men 0, Women 1, Non-binary 2
-  "race"=c("White", "Hispanic", "Hispanic", "White", "Black", "White", "White", "Black", "Hispanic", "White"), # Class?
+  "race"=c("White", "Hispanic", "Hispanic", "White", "White", "White", "White", "White", "Hispanic", "White"), # Class?
   "income"=c(25000, 50000, 28000, 60000, 19000, 20000, 33000, 77000, 50000, 64000), # Income per YEAR in USD
-  "ideology"=c(1, 3, 4, 5, 4, 5, 6, 7, 2, 4), # 7-point scale: left (1) and right (7)
-  "extraversion"=c(0.22, 0.14, 0.77, 1, 0.67, 0.45, 0.65, 0.5, 0.77, 0.33), # Ranges from 0 to 1
+  "ideology"=c(1, 3, 4, 5, 4, 5, 6, 7, 2, 4), # 7-point scale: extreme left (1) and extreme right (7)
+  "extraversion"=c(0.22, 0, 0.77, 1, 0.67, 0.45, 0.65, 0.5, 0.77, 0.33), # Ranges from 0 to 1
   "polknow"=c(2, 3, 4, 5, 6, 2, 0, 4, 3, 6), # Ranges from 0 to 6
   "vaccinated"=c(TRUE, FALSE, TRUE, FALSE, TRUE, FALSE, TRUE, TRUE, TRUE, TRUE), # Logical
   "vote2022"=c(FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE,  FALSE, TRUE), # Logical
@@ -66,64 +52,48 @@ minidf<-data.frame(
 
 # GET TO KNOW THE DATA!
 # # -------------------------------------------------------------------------------------------
+
+# Visualizing the data.frame() object
 minidf; print(minidf) #
 View(minidf) # 
 
-nrow(minidf) # 
-ncol(minidf) #
-
-# Let's expand our data.frame example:
-minidf <- minidf %>% dplyr::slice(rep(seq_len(nrow(.)), 250)) # 10 (nrow) * 250
-
 # -- Attributes functions
 names(minidf) # 
+nrow(minidf) # 
+ncol(minidf) #
 str(minidf) #
 
-nrow(minidf) 
-ncol(minidf)
-
-# -- Classes of variables
-is.logical(minidf$vote2022) # 
+# -- Class functions
+class(minidf$race)
 is.factor(minidf$race) # 
+is.logical(minidf$vote2022) # 
 !is.numeric(minidf$age) # 
 !is.character(minidf$name) # 
 
 # # -----------------------------------------------------------------------
-# USING R OPERATORS: DATAFRAMES & VARIABLES TRASNFORMATION
+# USING R OPERATORS: VARIABLES TRANSFORMATION
 # # -----------------------------------------------------------------------
-
-# Let's use some R operators to understand our data, then change a couple of
-# variables to make data interpretation easier depending on our goals.
+# Let's use some R operators to understand and transform our data.
 
 # --- Find how many Women are in the sample
 table(minidf$gender) # 
-prop.table(table(minidf$gender==1)) # 
-
-# --- Find how many people are vaccinated in the sample
-table(minidf$vaccinated==TRUE)
-prop.table(table(minidf$vaccinated))*100
 
 ## -- Find what's the individual income by month
 table(minidf$income)
 minidf$income_month<-minidf$income/12 # 
-table(minidf$income); table(minidf$income_month) # 
+
+table(minidf$income_month) # 
 
 # -- Find the number of respondents by income
-table(minidf$income<30000)
-table(minidf$income_month>=5000)
+table(minidf$income<30000) # 
+table(minidf$income_month>=5000) # 
+
+# --- Find the % people are vaccinated in the sample
+table(minidf$vaccinated==TRUE) # '==' operator
+prop.table(table(minidf$vaccinated))*100
 
 # # -------------------------------------------------------------------------------------------
-
-# Multiply "race" by 100
-minidf$race100<-minidf$race*100
-is.numeric(minidf$race); class(minidf$race)
-
-# -- Multiply "vote2022" by 100.
-class(minidf$vote2022)
-minidf$vote2022<-minidf$vote2022*100
-table(minidf$vote2022)
-
-# # -------------------------------------------------------------------------------------------
+# Now let's change a couple of variables to make data interpretation easier.
 
 # -- Reverse code "risk_averse" to "risk_taker"
 table(minidf$risk_averse)
@@ -136,21 +106,22 @@ minidf$introversion<-(minidf$extraversion*-1)+1
 table(minidf$introversion)
 
 # # -----------------------------------------------------------------------
-# Now, lets transform different variable classes to the factor type.
-# The factor type will make data use easier in future classes (i.e., modeling).
+# Now, lets transform variables to the factor data type.
+# The factor type will make data use easier in future classes 
+# (i.e., modeling or plotting).
 # # -----------------------------------------------------------------------
 
 # --- Transform ideology (numeric) into factor (categorical)
 # # -------------------------------------------------------------------------------------------
 table(minidf$ideology)
-minidf$ideo_factor<-factor(minidf$ideology, levels=c(1:7), 
-                           labels=c("Extreme Left",
+minidf$ideo_factor<-factor(minidf$ideology, levels=c(1,2,3,4,5,6,7), 
+                           labels=c("Extreme-left",
                                     "Left",
                                     "Center-left",
                                     "Independent",
                                     "Center-right",
                                     "Right",
-                                    "Extreme Right"))
+                                    "Extreme-right")) # 
 table(minidf$ideo_factor)
 
 # --- Transform vaccinated (logical) into factor (categorical)
@@ -162,14 +133,13 @@ table(minidf$vaccinated)
 
 # --- Transform race (character) into factor (categorical)
 # # -------------------------------------------------------------------------------------------
-class(minidf$race); table(minidf$race)
-minidf$race<-factor(minidf$race, levels=c("Black", "Hispanic", "White"))
+table(minidf$race); class(minidf$race)
+minidf$race<-factor(minidf$race, levels=c("Hispanic", "White"))
 is.factor(minidf$race)
-
-minidf$race100<-minidf$race*100 # 
+str(minidf$race)
 
 # # -----------------------------------------------------------------------
-### CODING EXERCISE
+### CODING EXERCISES
 # # -----------------------------------------------------------------------
 
 # --- # --- # --- # --- # --- # --- # --- # --- # --- # --- # --- # --- # ---
@@ -189,9 +159,9 @@ minidf$race100<-minidf$race*100 #
 # --- # --- # --- # --- # --- # --- # --- # --- # --- # --- # --- # --- # ---
 
 # The data show the score (from 0 to 20) for 20 different students, 
-# some of whom are male and some female, 
-# and some of whom were taught using **positive** reinforcement (i.e., being nice), 
-# and others who were taught using **negative** reinforcement (i.e., punishment). 
+# some of whom are male and some female, and some of whom were taught using 
+# **positive** reinforcement (i.e., being nice), and others who were taught using
+# **negative** reinforcement (i.e., punishment). 
 
         ### # -------------------------------------------------- # ###
         ### || ------- MALE ------- || ------- FEMALE ------- || # ###
@@ -204,14 +174,13 @@ minidf$race100<-minidf$race*100 #
         ### ---------------------------------------------------- # ###
 
 ### TIP: The data will not be entered in the same way that they are laid out above.
-### You will have to wrangle the table information above in 3 variables: "score",  
-### "gender", and "treatment"
+### You will have to wrangle the table information above in 3 variables:   
+### "score", "gender", and "treatment"
 
 # ANSWER:
 
-
 ###########################################################################
-# NEXT CLASS: 
+# --- NEXT CLASS: 
 # More on data structures & data transformation functions.
 # Reading in and exporting out data.
 ###########################################################################
