ef13a285e4594b6e2f3e7d017bb877119ee1f772
Robert Vidigal <rb5286@nyu.edu>
2023-03-06 16:26:39 -0500

    IntroR Class DEMO commit

--- IntroR_DEMO.R
+++ IntroR_DEMO.R
@@ -1,23 +1,20 @@
 ###########################################################################
 # Stevens Institute of Technology
 # College of Arts and Sciences
-# Purpose: Introduction to R - Class 2
+# Purpose: Introduction to R
 # Author: Robert Vidigal
 # Version: March 7th
-# Class materials available at: https://github.com/robertvidigal/IntroR-spring2023
-# RStudio is also available online: http://rstudio.cloud
+# Materials available at: https://github.com/robertvidigal/IntroR-spring2023
 ###########################################################################
 
 # # -----------------------------------------------------------------------
 # --- Today RStudio Topics ---
 # # -----------------------------------------------------------------------
 ### Entering data in R (variable types).
-### Using R operators with data frames (data structures).
+# 1. We will learn common variable types transformations.
 
-# # -----------------------------------------------------------------------
-# LET'S BEGIN!
-# # -----------------------------------------------------------------------
-rm(list=ls()) # Cleaning R Environment
+### Using R operators with data frames (data structures). 
+# 2. We will learn R functions to explore data frames structure.
 
 # # -----------------------------------------------------------------------
 # ENTERING DATA IN R: VARIABLE TYPES
@@ -35,25 +32,31 @@ rm(list=ls()) # Cleaning R Environment
 # Let's suppose we collected survey answers on some political, psychological, 
 # and demographic characteristics.
 
-### Inputting data manually
 minidf<-data.frame(
-  "name"=c("Sofia", "Pedro", "Juan", "Emma", "Fred", "Sasha", "Rachel", "Erica", "Amanda", "Maya"), # Individuals
+  "name"=c("Sofia", "Pedro", "Juan", "Emma", "Fred", 
+           "Sasha", "Rachel", "Erica", "Amanda", "Maya"), # Individuals
   "age"=c(23, 22, 44, 56, 90, 18, 77, 31, 20, 23), # Age in years
   "gender"=c(1, 0, 0, 1, 0, 0, 1, 1, 1, 2), # Men 0, Women 1, Non-binary 2
-  "race"=c("White", "Hispanic", "Hispanic", "White", "White", "White", "White", "White", "Hispanic", "White"), # Class?
-  "income"=c(25000, 50000, 28000, 60000, 19000, 20000, 33000, 77000, 50000, 64000), # Income per YEAR in USD
-  "ideology"=c(1, 3, 4, 5, 4, 5, 6, 7, 2, 4), # 7-point scale: extreme left (1) and extreme right (7)
-  "extraversion"=c(0.22, 0, 0.77, 1, 0.67, 0.45, 0.65, 0.5, 0.77, 0.33), # Ranges from 0 to 1
+  "ethnicity"=c("Non-hispanic", "Hispanic", 
+                "Non-hispanic", "Non-hispanic", 
+                "Non-hispanic", "Non-hispanic", 
+                "Non-hispanic", "Non-hispanic", 
+                "Hispanic", "Non-hispanic"), # Hispanic Ethnicity
+  "income"=c(25000, 50000, 28000, 60000, 19000, 
+             25000, 33000, 77000, 50000, 64000), # Income per YEAR in USD
+  "ideology"=c(1, 3, 4, 5, 4, 5, 6, 7, 2, 4), # 7-point: extreme left (1) to extreme right (7)
+  "extraversion"=c(0.22, 0, 0.77, 1, 0.67, 0.45, 0.65, 0.5, 0.77, 0.22), # Ranges from 0 to  1
   "polknow"=c(2, 3, 4, 5, 6, 2, 0, 4, 3, 6), # Ranges from 0 to 6
   "vaccinated"=c(TRUE, FALSE, TRUE, FALSE, TRUE, FALSE, TRUE, TRUE, TRUE, TRUE), # Logical
   "vote2022"=c(FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE,  FALSE, TRUE), # Logical
   "risk_averse"=c(1, 4, 1, 4, 4, 0, 3, 2, 3, 3) # Ranges from 0 to 4
 )
 
-# GET TO KNOW THE DATA!
+# # -------------------------------------------------------------------------------------------
+# LET'S GET TO KNOW THE DATA
 # # -------------------------------------------------------------------------------------------
 
-# Visualizing the data.frame() object
+# The data.frame() object
 minidf; print(minidf) #
 View(minidf) # 
 
@@ -75,32 +78,33 @@ is.logical(minidf$vote2022) #
 # # -----------------------------------------------------------------------
 # Let's use some R operators to understand and transform our data.
 
-# --- Find how many Women are in the sample
+# --- Find how many Women are in the sample.
+minidf$gender
 table(minidf$gender) # 
 
-## -- Find what's the individual income by month
+## -- Find what's the individual income by month.
 table(minidf$income)
-minidf$income_month<-minidf$income/12 # 
-
+minidf$income_month<-minidf$income/12
 table(minidf$income_month) # 
 
-# -- Find the number of respondents by income
-table(minidf$income<30000) # 
-table(minidf$income_month>=5000) # 
+# -- Find the % of respondents by yearly and monthly income threshold.
+minidf$income<30000 # Logical operator
+table(minidf$income<30000) # Counts
+proportions(table(minidf$income<30000)) # Proportions
+proportions(table(minidf$income<30000))*100 # Percentages
 
-# --- Find the % people are vaccinated in the sample
-table(minidf$vaccinated==TRUE) # '==' operator
-prop.table(table(minidf$vaccinated))*100
+# --- Find the % people are Hispanic in the sample.
+table(minidf$ethnicity=="Hispanic") # '==' operator
+table(minidf$ethnicity!="Non-hispanic") # '!=' operator
 
-# # -------------------------------------------------------------------------------------------
 # Now let's change a couple of variables to make data interpretation easier.
-
-# -- Reverse code "risk_averse" to "risk_taker"
+# # -------------------------------------------------------------------------------------------
+# -- Reverse code "risk_averse" to "risk_taker" (higher values indicate more risk-taking).
 table(minidf$risk_averse)
 minidf$risk_taker<-(minidf$risk_averse*-1)+4
 table(minidf$risk_taker)
 
-## -- Reverse code "extraversion" to "introversion"
+## -- Reverse code "extraversion" to "introversion" (higher values indicate more introvert).
 table(minidf$extraversion)
 minidf$introversion<-(minidf$extraversion*-1)+1
 table(minidf$introversion)
@@ -111,7 +115,7 @@ table(minidf$introversion)
 # (i.e., modeling or plotting).
 # # -----------------------------------------------------------------------
 
-# --- Transform ideology (numeric) into factor (categorical)
+# --- Transform ideology (numeric) into factor (categorical).
 # # -------------------------------------------------------------------------------------------
 table(minidf$ideology)
 minidf$ideo_factor<-factor(minidf$ideology, levels=c(1,2,3,4,5,6,7), 
@@ -124,19 +128,21 @@ minidf$ideo_factor<-factor(minidf$ideology, levels=c(1,2,3,4,5,6,7),
                                     "Extreme-right")) # 
 table(minidf$ideo_factor)
 
-# --- Transform vaccinated (logical) into factor (categorical)
+# --- Transform vaccinated (logical) into factor (categorical).
 # # -------------------------------------------------------------------------------------------
 table(minidf$vaccinated)
 minidf$vaccinated<-factor(minidf$vaccinated, levels=c(TRUE, FALSE),
                           labels=c("Vaccinated", "Non-vaccinated"))
 table(minidf$vaccinated)
 
-# --- Transform race (character) into factor (categorical)
+# --- Transform ethnicity (character) into factor (categorical).
 # # -------------------------------------------------------------------------------------------
-table(minidf$race); class(minidf$race)
-minidf$race<-factor(minidf$race, levels=c("Hispanic", "White"))
-is.factor(minidf$race)
-str(minidf$race)
+table(minidf$ethnicity); class(minidf$ethnicity)
+minidf$hispanic<-factor(minidf$ethnicity, levels=c("Non-hispanic", "Hispanic"))
+is.factor(minidf$hispanic)
+str(minidf$hispanic)
+
+minidf$hispanic
 
 # # -----------------------------------------------------------------------
 ### CODING EXERCISES
@@ -184,3 +190,16 @@ str(minidf$race)
 # More on data structures & data transformation functions.
 # Reading in and exporting out data.
 ###########################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+#minidf <- minidf %>% dplyr::slice(rep(seq_len(nrow(.)), 250)) # 10 (nrow) * 250
